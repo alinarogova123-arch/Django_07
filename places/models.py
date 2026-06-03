7a47b7bb807c52ff19b64d95f275bb5d934d6e02
alinarogova123-arch <alinarogova123@gmail.com>
2026-06-03 20:26:37 +0300

    Add model for images

--- places/models.py
+++ places/models.py
@@ -10,3 +10,16 @@ class Place(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Image(models.Model):
+    img = models.ImageField(upload_to='images/')
+    place = models.ForeignKey(Place, on_delete=models.CASCADE, related_name="images")
+    order = models.PositiveIntegerField(default=0, db_index=True)
+
+    class Meta:
+
+        ordering = ['order']
+
+    def __str__(self):
+        return f"{self.order} {self.place.title}"
