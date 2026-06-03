e8e085c2ddb751a2163fee0aa1d063ec5b397468
alinarogova123-arch <alinarogova123@gmail.com>
2026-06-03 23:32:27 +0300

    Move data from template to view

--- places/views.py
+++ places/views.py
@@ -1,5 +1,31 @@
 from django.shortcuts import render
+from .models import Place, Image
 
 # Create your views here.
+
+
+def serialize_place(place):
+    return {
+      'type': 'Feature',
+      'geometry': {
+        'type': 'Point',
+        'coordinates': [place.lon, place.lat]
+      },
+      'properties': {
+        'title': place.title,
+        'placeId': place.id,
+        'detailsUrl': './static/places/moscow_legends.json'
+      }
+    }
+
+
 def index(request):
-	return render(request, 'index.html')
+	places = Place.objects.all()
+	context = {
+		'places': {
+			'type': 'FeatureCollection',
+      		'features': [serialize_place(place) for place in places],
+		},
+	}
+
+	return render(request, 'index.html', context)
