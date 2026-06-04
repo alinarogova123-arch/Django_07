c77490b30ae310f804d5ed413437fa9a36c54a36
alinarogova123-arch <alinarogova123@gmail.com>
2026-06-04 17:53:47 +0300

    added WYSIWYG editor for long description

--- places/models.py
+++ places/models.py
@@ -1,10 +1,11 @@
 from django.db import models
+from tinymce.models import HTMLField
 
 # Create your models here.
 class Place(models.Model):
     title = models.CharField(max_length=200)
     description_short = models.TextField()
-    description_long = models.TextField()
+    description_long = HTMLField()
     lat = models.FloatField()
     lon = models.FloatField()
 
--- where_to_go/urls.py
+++ where_to_go/urls.py
@@ -2,11 +2,12 @@ from places import views
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
     path('places/<int:place_id>/', views.places_detail, name='places'),
+    path('tinymce/', include('tinymce.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
