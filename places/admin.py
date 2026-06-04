dca72f873b738717f4d66ee3c816c0687d6a7d08
alinarogova123-arch <alinarogova123@gmail.com>
2026-06-04 16:26:18 +0300

    added image preview

--- places/admin.py
+++ places/admin.py
@@ -1,10 +1,22 @@
 from django.contrib import admin
+from django.utils.html import format_html
 from .models import Place, Image
 
 
 class ImageInline(admin.TabularInline):
     model = Image
     extra = 1
+    fields = ["img", "preview_image", "order"]
+    readonly_fields = ['preview_image']
+
+    def preview_image(self, obj):
+        size_attitude = obj.img.width / obj.img.height
+        return format_html('<img src="{url}" width="{width}" height={height} />'.format(
+            url = obj.img.url,
+            width=200 * size_attitude,
+            height=200,
+            )
+    )
 
 
 @admin.register(Place)
