de47202e1ff229acc5740f5e7934d6cc440be7d7
alinarogova123-arch <alinarogova123@gmail.com>
2026-06-07 14:41:18 +0300

    added blank=True for description fields

--- places/models.py
+++ places/models.py
@@ -4,8 +4,8 @@ from tinymce.models import HTMLField
 
 class Place(models.Model):
     title = models.CharField(max_length=200)
-    description_short = models.TextField()
-    description_long = HTMLField()
+    description_short = models.TextField(blank=True)
+    description_long = HTMLField(blank=True)
     lat = models.FloatField()
     lon = models.FloatField()
 
