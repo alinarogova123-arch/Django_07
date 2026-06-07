0c27b81fcdac1c82d5b0d8f6ac4865abf4913093
alinarogova123-arch <alinarogova123@gmail.com>
2026-06-07 15:16:48 +0300

    added verbose name for fields

--- places/models.py
+++ places/models.py
@@ -3,25 +3,35 @@ from tinymce.models import HTMLField
 
 
 class Place(models.Model):
-    title = models.CharField(max_length=200)
-    short_description = models.TextField(blank=True)
-    long_description = HTMLField(blank=True)
-    lat = models.FloatField()
-    lon = models.FloatField()
+    title = models.CharField(max_length=200, verbose_name="Название")
+    short_description = models.TextField(blank=True, verbose_name="Краткое описание")
+    long_description = HTMLField(blank=True, verbose_name="Полное описание")
+    lat = models.FloatField(verbose_name="Широта")
+    lon = models.FloatField(verbose_name="Долгота")
 
     def __str__(self):
         return self.title
 
 
 class Image(models.Model):
-    name = models.CharField(max_length=200, null=True, blank=True)
-    img = models.ImageField(upload_to='images/')
-    place = models.ForeignKey(Place, on_delete=models.CASCADE, related_name="images")
-    order = models.PositiveIntegerField(default=0, db_index=True)
+    name = models.CharField(
+        max_length=200,
+        null=True,
+        blank=True,
+        verbose_name="Имя фотографии"
+    )
+    img = models.ImageField(upload_to='images/', verbose_name="Фотография")
+    place = models.ForeignKey(
+        Place,
+        on_delete=models.CASCADE,
+        related_name="images",
+        verbose_name="Локация"
+    )
+    order = models.PositiveIntegerField(default=0, db_index=True, verbose_name="Порядковый номер")
 
     class Meta:
 
         ordering = ['order']
 
     def __str__(self):
-        return f"{self.order} {self.place.title}"
+        return f"{self.place.title}"
