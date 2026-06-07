fcead741f986cacff22c46124d94273d4dec601b
alinarogova123-arch <alinarogova123@gmail.com>
2026-06-07 18:40:14 +0300

    Update load_place.py

--- places/management/commands/load_place.py
+++ places/management/commands/load_place.py
@@ -9,8 +9,8 @@ from places.models import Place, Image
 
 
 def add_image_to_place(image_urls, place):
-	attempt = 0
-	for image_url in image_urls:
+    attempt = 0
+    for image_url in image_urls:
         try:
             response = requests.get(image_url)
             response.raise_for_status()
@@ -21,37 +21,37 @@ def add_image_to_place(image_urls, place):
             if attempt > 10:
                 time.sleep(20)
         else:
-        	attempt = 0
-			image = BytesIO(response.content)
-			image_file = File(image, name=Path(image_url).name)
-			img, created = Image.objects.get_or_create(
-				name=Path(image_url).name,
-				place=place,
-				defaults={
-        			'img': image_file
-    			}
-			)
+            attempt = 0
+            image = BytesIO(response.content)
+            image_file = File(image, name=Path(image_url).name)
+            img, created = Image.objects.get_or_create(
+                name=Path(image_url).name,
+                place=place,
+                defaults={
+                    'img': image_file
+                }
+            )
 
 
 class Command(BaseCommand):
-	help = "Добавляет запись в модель Place"
+    help = "Добавляет запись в модель Place"
 
 
-	def add_arguments(self, parser):
-		parser.add_argument("url", type=str, help="url JSON файла")
+    def add_arguments(self, parser):
+        parser.add_argument("url", type=str, help="url JSON файла")
 
 
-	def handle(self, *args, **options):
-		url = options["url"]
-		response = requests.get(url)
-		response.raise_for_status()
-		serialize_place = response.json()
-		image_urls = serialize_place.get("imgs")
-		place, created = Place.objects.get_or_create(
-			title=serialize_place.get("title"),
-    		short_description=serialize_place.get("description_short"),
-    		long_description=serialize_place.get("description_long"),
-    		lat=serialize_place.get("coordinates").get("lat"),
-    		lon=serialize_place.get("coordinates").get("lng"),
-    	)
-		add_image_to_place(image_urls, place)
+    def handle(self, *args, **options):
+        url = options["url"]
+        response = requests.get(url)
+        response.raise_for_status()
+        serialize_place = response.json()
+        image_urls = serialize_place.get("imgs")
+        place, created = Place.objects.get_or_create(
+            title=serialize_place.get("title"),
+            short_description=serialize_place.get("description_short"),
+            long_description=serialize_place.get("description_long"),
+            lat=serialize_place.get("coordinates").get("lat"),
+            lon=serialize_place.get("coordinates").get("lng"),
+        )
+        add_image_to_place(image_urls, place)
